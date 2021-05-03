c70441ab112682bfeff5d49ea32916e6e1c76e3f
ndaemy <yuyaebean@gmail.com>
2021-05-03 17:35:21 +0900

    1.4 InputTypes and ArgsType

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
-import { Restaurant } from '../entities/restaurant.entity';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 
 @Resolver()
 export class RestaurantsResolver {
@@ -7,5 +8,11 @@ export class RestaurantsResolver {
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     console.log(veganOnly);
     return [];
+  }
+
+  @Mutation(returns => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
   }
 }
