5bf84ecaeafe7f2bd9a20a1d2fe0c8f07c2049da
ndaemy <yuyaebean@gmail.com>
2021-05-08 15:11:23 +0900

    3.2 Injecting The Repository

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -2,13 +2,15 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantsService } from './restaurants.service';
 
 @Resolver()
 export class RestaurantsResolver {
+  constructor(private readonly restaurantService: RestaurantsService) {}
+
   @Query(returns => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log(veganOnly);
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
 
   @Mutation(returns => Boolean)
---
+++ src/restaurants/restaurants.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+import { Restaurant } from './entities/restaurant.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class RestaurantsService {
+  constructor(
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
+  ) {}
+
+  getAll(): Promise<Restaurant[]> {
+    return this.restaurants.find();
+  }
+}
