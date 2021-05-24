138338f821ec1cf8eb2d7cca5f5f6075987e1dd0
ndaemy <yuyaebean@gmail.com>
2021-05-24 19:18:53 +0900

    4.5 Create Account Mutation part 3

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,19 +11,19 @@ export class UsersService {
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
 
-  async createAccount({ email, password, role }: CreateAccountInput) {
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
-        // make error
-        return;
+        return 'There is a user with that email already';
       }
       await this.users.save(this.users.create({ email, password, role }));
-      return true;
     } catch (e) {
-      return;
+      return "Couldn't create account";
     }
-    // check new user
-    // create user & hash the password
   }
 }
